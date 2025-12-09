4dc858eb097f042cfa3556b45166b0b044ff5bb4
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2025-12-09 14:21:14 +0545

    Enable printing on screen

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,23 @@
 #![no_main] // don't use main function as entry point
 use core::panic::PanicInfo;
 
-// function called on panic
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+static MESSAGE: &[u8] = b"Sano OS";
 
 // entry point
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in MESSAGE.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    loop {}
+}
+
+// function called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
